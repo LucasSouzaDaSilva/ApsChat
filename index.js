f65603429c6ff3b4439eceef69031bc58b55c76a
Diego Fernandes <diego.oliveira@b4a.com.br>
2021-09-27 21:46:36 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+var express = require('express')
+var app = express()
+var http = require('http').createServer(app)
+var io = require("socket.io")(http)
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+  res.render('chat')
+})
 
 app.get('/home', function(req, res){
   res.render('home')
-})
-
-app.get('/login', function(req, res){
-  res.render('login')
 })
 
 app.get('/cadastro', function(req, res){
   res.render('cadastro')
 })
 
-app.listen(4040, () => {
-  console.log("app rodando...");
-}); // inicializando a minha aplicação http://localhost:4040
+app.get('/login', function(req, res){
+  res.render('login')
+})
+
+io.on("connection",(client) => {
+  client.on("disconnect", () => {
+    console.log("X desconectou: " + client.id)
+  })
+
+  client.on("palavra", (data) => {
+    console.log(data)
+    client.emit("resultado", data + " " + "Grenner App" )
+  })
+
+  client.on('msg', (data) => {
+    client.emit("showmsg", data)
+    console.log(data)
+  })
+})
+
+http.listen(3000, () => {
+  console.log('APP RODANDO!')
+})
+
