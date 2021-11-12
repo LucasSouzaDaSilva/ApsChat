6db0f94f44f1625f118fdbcde9042db98a3f961e
LucasSouzaDaSilva <lucas.silva@accountfy.com>
2021-11-12 20:28:56 -0300

    Servidor rodando com as rotas. Pendente consertar o bodyparse

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
-var express = require('express')
-var app = express()
-var http = require('http').createServer(app)
-var io = require("socket.io")(http)
+const express = require("express");
+const session = require("express-session");
+const BodyParser = require("body-parser");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.set("view engine", "ejs")
+var login = "admin";
+var senha = "1234";
 
-app.get('/', (req, res) => {
-  res.render('chat')
-})
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use(session({ secret: "sadfsdfsdfsd74328r73489" }));
+app.use(BodyParser.urlencoded({ extended: true }));
 
-app.get('/home', function(req, res){
-  res.render('home')
-})
+app.post("/", (req, res) => {
+  
+  if (req.body.senha == senha && req.body.login == login) {
+    //Logado!
+    req.session.login = login;
 
-app.get('/cadastro', function(req, res){
-  res.render('cadastro')
-})
+    res.render("home.ejs");
 
-app.get('/login', function(req, res){
-  res.render('login')
-})
+  } else {
+    res.render("login.ejs");
+  }
 
-io.on("connection",(client) => {
+});
+app.get("/", function (req, res) {
+  if(req.session.login){
+    res.render('home.ejs')
+    
+  }else{
+    res.render("login.ejs");
+  }
+
+  
+});
+
+app.get("/home", function (req, res) {
+  res.render("home.ejs");
+});
+
+app.get("/cadastro", function (req, res) {
+  res.render("cadastro.ejs");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login.ejs");
+});
+
+io.on("connection", (client) => {
   client.on("disconnect", () => {
-    console.log("X desconectou: " + client.id)
-  })
+    console.log("X desconectou: " + client.id);
+  });
 
   client.on("palavra", (data) => {
-    console.log(data)
-    client.emit("resultado", data + " " + "Grenner App" )
-  })
+    console.log(data);
+    client.emit("resultado", data + " " + "Grenner App");
+  });
 
-  client.on('msg', (data) => {
-    client.emit("showmsg", data)
-    console.log(data)
-  })
-})
+  client.on("msg", (data) => {
+    client.emit("showmsg", data);
+    console.log(data);
+  });
+});
 
 http.listen(3000, () => {
-  console.log('APP RODANDO!')
-})
+  console.log("APP RODANDO!");
+});
 
